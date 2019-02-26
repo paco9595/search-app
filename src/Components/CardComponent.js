93e22db461c8cf794c9039bb7b9dca437aeb41c3
Francisco Villarreal <francisco.j.padilla@accenture.com>
2019-02-26 14:21:53 -0600

    create Card Component

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -5,24 +5,26 @@ const CardContainer = styled.div`
     width: 300px;
     height: 200px;
     background-color: white;
-    box-shadow: 17px 6px 5px 0px rgba(0,0,0,0.75);
+    ${props => props.shadow ? 'box-shadow: 5px 5px 20px 0px rgba(0,0,0,0.75);' : null}
+    margin: 20px;
 `
 const CardImg = styled.img`
     width: 300px;
     height: 100px;
     border-bottom: 1px solid black;
+    border-right: 1px solid black;
 `
 const CardTitle = styled.div`
     font-size: 20px;
 `
 const CardDescription = styled.div``
 export const Card = props => {
-    const { nombre, img, Descripcion } = props;
+    const { nombre, img, descripcion, shadow, click } = props;
     return (
-        <CardContainer>
+        <CardContainer shadow={shadow} onClick={click}>
             <CardImg src={img} />
             <CardTitle>{nombre}</CardTitle>
-            <CardDescription>{Descripcion}</CardDescription>
+            <CardDescription>{descripcion}</CardDescription>
         </CardContainer>
     )
 }
